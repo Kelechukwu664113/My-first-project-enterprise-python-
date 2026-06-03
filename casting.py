880dba914ff716e36a109aaa20ecc69117498bdb
Kelechukwu664113 <kelechukwujohne@gmail.com>
2026-06-03 14:37:19 +0200

    changes

--- casting.py
+++ casting.py
@@ -1,7 +1,23 @@
-members_average = 25.5
+"""members_average = 25.5
 x = str(members_average)
 
 print(x)
 print(type(x))
 
+x,y,z = "orange", "apple", "pear"
+print(x)
+print(y)
+print(z)
 
+"""
+name = "kelechukwu"
+
+for x in name:
+    print(x)
+
+    
+
+print(len(name))
+
+statement = "obi is a boy"
+print("b" in statement)
